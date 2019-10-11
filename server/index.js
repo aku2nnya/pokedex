3b5bff0ba2ce7097f557e48d51dc56b0a45a8b5f
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-10-10 20:14:12 -0700

    Add public static folder usage

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const { getPokemonData } = require('./controller');
 const app = express();
 const PORT = 3000;
 
-app.route('/pokemon/:name')
-  .get(getPokemonData);
+app.use(express.static('public'));
+
+app.get('/pokemon/:name', getPokemonData);
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
