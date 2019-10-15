756e7da6bdf6ecb51edc7ee9df924e679b110e0c
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-10-15 15:33:23 -0700

    Implement basic info display upon search

--- public/scripts.js
+++ public/scripts.js
@@ -1,11 +1,35 @@
 const searchButton = document.querySelector('#searchButton');
+const pokemonInfo = document.querySelector('#pokemonInfo');
+
+
+function displayPokemonInfo(data) {
+  pokemonInfo.innerHTML = `
+    <img src=${data.sprites.front_default} alt="Pokemon Image">
+    <div>Name: ${data.name}</div>
+    <div>Height: ${data.height}</div>
+    <div>Weight: ${data.weight}</div>
+  `;
+}
 
 function getPokemonData(e) {
-  const inputText = e.target.form[0].value;
+  const inputText = e.target.form[0].value.toLowerCase();
   axios.get(`pokemon/${inputText}`)
-    .then((response) => console.log(response.data))
-    .catch((error) => console.log(error.response.data));
+    .then((response) => displayPokemonInfo(response.data))
+    .catch((error) => {
+      if (error.response.status === 404) {
+        pokemonInfo.innerHTML = `
+          <br>
+          <div>${error.response.data}</div>
+          `;
+      } else {
+        pokemonInfo.innerHTML = `
+          <br>
+          <div>${error}</div>
+          `;
+      }
+    });
   e.preventDefault();
 }
+
 
 searchButton.addEventListener('click', getPokemonData);
